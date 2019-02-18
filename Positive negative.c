2df390ddec65d30b1167850b550a44d6368364f4
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-02-18 19:28:40 +0530

    Create Positive negative.c

---
+++ Positive negative.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int num;
+printf(“enter the elements”);
+scanf(“%d”,&num);
+if(num>0)
+printf(“%d is a positive num”);
+else if(num<0)
+printf(“%d is a negative num”);
+else if
+printf(“%d is a zero”);
+return 0;
+}
+}
